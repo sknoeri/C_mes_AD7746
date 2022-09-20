dd4fc618b76e34519f4651a96aaa35c0c84c43b3
Simon <simon.knori@epfl.ch>
2022-09-20 12:32:41 -0700

    Need to creat a configratin file Config_AD7746 to configur the capacitance reader

---
+++ Config_AD7746/Config_AD7746.ino
@@ -0,0 +1,45 @@
+#include <Wire.h>
+
+//I/O pin configuration registers
+#define DIRD *((volatile unsigned char*) 0x2A) // Data dircetion register of port D
+#define PIND *((volatile unsigned char*) 0x29) // Pin set register of port D
+#define DIRB *((volatile unsigned char*) 0x24) // Data dircetion register of port B
+#define PINB *((volatile unsigned char*) 0x23) // Pin set register of port B
+
+// Timer0 configuration register
+#define TCCR2A *((volatile unsigned char*) 0xB0) // Timer register A
+#define TCCR2B *((volatile unsigned char*) 0xB1) // Timer register B
+#define TIMSK2 *((volatile unsigned char*) 0x70) // Timer interrup enable register
+
+
+void setup() {
+  DIRB=0;
+  PINB=0;
+  DIRB|=(1<<5); // Sets direction register for PB5
+  PINB|=(0<<5);
+  initTimer2();
+}
+
+void loop() {
+  ///digitalWrite(6,1);
+  //analogWrite(6,100); 
+  PINB^=(1<<5);
+  delay(1000);                       // wait for a second
+}
+
+void initTimer2(){
+  TCCR2A|=(0<<7)|(0<<6);          // Timer in normal operation, OC0A disconected
+  TCCR2A|=(0<<0)|(0<<1);          // Timer in normal opperation counts until 0xFF and sets TOV flag there
+  TCCR2B|=(0<<3);          // Timer in normal opperation counts until 0xFF and sets TOV flag there continuation of TCCR0A
+  TCCR2B|=(1<<2)|(1<<1)|(1<<0);          // No prescaling takes clock frequenzie
+  TIMSK2|=(1<<0);          // Enalbs the overflow interuppt
+}
+unsigned char b=0;
+ISR(TIMER2_OVF_vect){
+  b++;
+  if(b==10){
+    b=0;
+    //PINB|=32; // Toggels PB5 
+    //digitalWrite(13,1);
+  }
+}
