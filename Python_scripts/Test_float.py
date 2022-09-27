d20aab98357e2b5d37d9153128ae9fb1ac2f1c65
Simon <simon.knori@epfl.ch>
2022-09-27 11:06:28 -0700

    debuggig grabage

--- Python_scripts/Test_float.py
+++ Python_scripts/Test_float.py
@@ -1,7 +1,8 @@
-import matplotlib.pyplot as plt
-
+import pandas as pd
 string = "23aasad039"
-
+a = [1,2,3,4,454,65]
+data=pd.DataFrame(a)
+data.to_csv('test.csv')
 print("The entered string : " + str(string))
 
 r = string.replace('.', '', 1).isnumeric()
--- Python_scripts/asd.py
+++ Python_scripts/asd.py
@@ -16,7 +16,7 @@ time = []
 t = 0
 cond = True
 plt.ion()
-
+alpfhabet = ['a','b','c','d','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','+','!','*','?','/','$']
 
 def plot():
     plt.title('Serial value from Arduino')
