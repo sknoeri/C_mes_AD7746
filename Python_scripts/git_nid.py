07acf45835678a0b80f1c77aaba89798c3e9e1b2
Simon <simon.knori@epfl.ch>
2022-10-03 18:24:21 -0700

    GUI with Serial On Off, Plot On off; Record on Off, save and input tex buttons

--- Python_scripts/git_nid.py
+++ Python_scripts/git_nid.py
@@ -16,9 +16,10 @@ serialInst = serial.Serial()
 pressure = []
 t_val = []
 # conditions to start plotting or to stop it and to register measurements
-cond = False
-cond2 = False
-plotStrat = True
+recordStop = True
+onOffPlot = False
+comOpen = False
+
 
 # function that plots the Serial data from the arduino
 def plot(t,data):
@@ -32,17 +33,23 @@ def plot(t,data):
     plt.show()
     plt.pause(0.001)
 # Fuction triggered by GUI
-def plot_start(): # starts measurements when the start btton is applied
+def record_data(): # starts measurements when the start btton is applied
     t = 0
     pressure = []
     t_val = []
-    global cond
-    cond = False
-    plt.close()
-    print("Start measurements")
+    global recordStop
+    if(recordStop == True and comOpen != False):
+        recordStop =False
+        plt.close()
+        print("Start measurements")
+    else:
+        recordStop = True
     while True: # infinite loop that reads the serial buffer of the Arduino and starts the plot
-        if cond == True: # global variable that can interupt the measuremetn
-            print("Rcording is interruped: restart file")
+        if(recordStop == True or comOpen == False): # global variable that can interupt the measuremetn
+            if comOpen == False:
+                print("No arduino Uno connected")
+            else:
+                print("Rcording is interruped")
             break
         if serialInst.in_waiting: # waits until charcters recived from Arduino
             t = t + Ts
@@ -56,26 +63,32 @@ def plot_start(): # starts measurements when the start btton is applied
             else: # append data vector
                 pressure.append(float(packet))
                 t_val.append(t)
-            if cond2 == False:
+            if onOffPlot == True: ## hadels the start and stop of the data plot
                 a = len(t_val)
                 plot(t_val[a-200:a], pressure[a-200:a]) # actualize the plot after every reading
-    #cond = False
-def stop_plot():
-    global cond2
-    cond2 ^= True
+            else:
+                #plt.clf()
+                root.update()
+# Turns on/off the plot
+def OnOff_plot():
+    global onOffPlot
+    if onOffPlot== False:
+        onOffPlot = True
+    else:
+        onOffPlot = False
 
-def rec_data(): # stp button is applyed the fucktion saves data to measurment file
-    global cond
-    export = input("Save measurments? y/n") # ask user if he want to measure the ouput
-    if export == 'y':
-        name = input("Name of .xlsx file:")
+def save_data(): # stp button is applyed the fucktion saves data to measurment file
+    if (len(t_val)>0):
+        if(inputtxt.get()==''):
+            name = 'NoName'
+        else:
+            name = inputtxt.get()
         data = pd.DataFrame({'time': t_val, 'pressure': pressure})
         Path = '/Users\simon\Documents\Simels_daten\Epfl\sem_13_2022_Master_theis_USA\Master_thesis\Capacitance_measuring\C_mes_AD7746'
-        data.to_excel(Path+'/data.xlsx')
+        data.to_excel(Path+'/' + name +'.xlsx')
         print("Register measurements")
     else:
-        print("Measurements not saved")
-    cond = True
+        print("Emty file")
 
 # fuction that searches if and where a Arduino is connected
 def serialPortArduino(): # Searches if Arduino Uno is connecto or not
@@ -99,40 +112,53 @@ def serialPortArduino(): # Searches if Arduino Uno is connecto or not
         else:
             print("No Arduino Uno conected")
     return portVar # false or portname
-
+########### Initialise the Serial port if Arduino is connected
+def comActive():
+    global comOpen
+    portVar = serialPortArduino()
+    if portVar == False:
+        comOpen = False
+        serialInst.close()
+    else:
+        serialInst.baudrate = 115200
+        serialInst.port = portVar
+        serialInst.open()
+        serialInst.reset_input_buffer()
+        comOpen = True
 
 #------ Creat buttons of little user interface to control the plot starts etc
 root = tk.Tk()
 root.title('Real time plot')
 root.config(background= 'light blue')
-root.geometry('200x50') #set the window size
-
-# Start Button
-root.update()
-start = tk.Button(root, text= "Start", font= ('calbiri',12), command= lambda: plot_start())
-start.place(x= 10, y=10)
-
-# Stop button
-root.update()
-record = tk.Button(root, text= "Save", font= ('calbiri',12), command= lambda: rec_data())
-record.place(x= start.winfo_x()+start.winfo_reqwidth() + 20, y= 10)
+root.geometry('400x200') #set the window size
 
 # Register data button
 root.update()
-stop = tk.Button(root, text= "Stop", font= ('calbiri',12), command= lambda: stop_plot())
-stop.place(x= record.winfo_x()+record.winfo_reqwidth() + 20, y= 10)
-root.update()
-########### Initialise the Serial port if Arduino is connected
-portVar = serialPortArduino()
-if portVar == False:
-    cond = True
-else:
-    serialInst.baudrate = 115200
-    serialInst.port = portVar
-    serialInst.open()
-    serialInst.reset_input_buffer()
-if portVar: # if Arduino is connected starts the GUI in loop
-    root.mainloop()
+startSerial = tk.Button(root, text= "Start Serial", font= ('calbiri',12), command= lambda: comActive())
+startSerial.place(x= 10, y=10)
+startSerial.update()
 
+# Start Button
+root.update()
+record = tk.Button(root, text="Record On/Off", font= ('calbiri', 12), command= lambda: record_data())
+record.place(x= 10, y=startSerial.winfo_y()+40)
+
+# Plot on off
+root.update()
+plotOnOff = tk.Button(root, text= "Plot On/Off", font= ('calbiri',12), command= lambda: OnOff_plot())
+plotOnOff.place(x= 10, y= record.winfo_y()+40)
+
+# save data button
+root.update()
+save = tk.Button(root, text= "Save", font= ('calbiri',12), command= lambda: save_data())
+save.place(x=plotOnOff.winfo_x(), y=plotOnOff.winfo_reqwidth() + 40)
+
+# tell ui the name of the saved file
+root.update()
+inputtxt = tk.Entry(root,width=20, bg="black", fg="white", borderwidth=5)
+inputtxt.place(x=save.winfo_x()+save.winfo_width()+20,y=save.winfo_y()+2)
+root.update()
+
+root.mainloop()
 
 
