93c6d5b186bad647137da4938b6aa4ed3c545595
Simon <simon.knori@epfl.ch>
2022-10-07 08:14:03 -0700

    resolved bug with save the measurements GUI fully functions, could add path text fied to define where to store measurements

--- Python_scripts/git_nid.py
+++ Python_scripts/git_nid.py
@@ -37,23 +37,26 @@ def plotFunc(t,data):
 # Fuction triggered by GUI
 def record_data(): # starts measurements when the start btton is applied
     t = 0
-    pressure = []
-    t_val = []
     global recordStop
-    if(recordStop == True and comOpen != False):
+    global t_val
+    global pressure
+    if(recordStop == True and comOpen != False):# Starts record when COM is open and recod is enabled by GUI
         recordStop =False
         plt.close()
-        record.config(image=on)
-        Output.insert("1.0","Start measurements \n")
+        record.config(image=on) # shows the ON button
+        Output.insert("1.0","Start measurements \n") # puts string a beginning of text field
+        pressure = []
+        t_val = []
     else:
         recordStop = True
-        record.config(image=off)
+        record.config(image=off) # shows the off button
     while True: # infinite loop that reads the serial buffer of the Arduino and starts the plot
         if(recordStop == True or comOpen == False): # global variable that can interupt the measuremetn
             if comOpen == False:
-                Output.insert("1.0","No arduino Uno connected \n")
+                Output.insert("1.0","No arduino Uno connected \n") # puts string a beginning of text field
             else:
-                Output.insert("1.0","Rcording is interruped \n")
+                Output.insert("1.0","Rcording is interruped \n") # puts string a beginning of text field
+                print(len(t_val))
             break
         if serialInst.in_waiting: # waits until charcters recived from Arduino
             t = t + Ts
@@ -63,7 +66,7 @@ def record_data(): # starts measurements when the start btton is applied
             if float_or_not == False: # if not float append 0 to data vector
                 pressure.append(0)
                 t_val.append(t)
-                Output.insert("1.0",packet +'\n')
+                Output.insert("1.0",packet +'\n') # puts string a beginning of text field
             else: # append data vector
                 pressure.append(float(packet))
                 t_val.append(t)
@@ -77,10 +80,10 @@ def record_data(): # starts measurements when the start btton is applied
 def OnOff_plot():
     global onOffPlot
     if onOffPlot== False:
-        plotOnOff.config(image=on)
+        plotOnOff.config(image=on) # shows the ON button
         onOffPlot = True
     else:
-        plotOnOff.config(image=off)
+        plotOnOff.config(image=off) # shows the off button
         onOffPlot = False
 
 def save_data(): # stp button is applyed the fucktion saves data to measurment file
@@ -89,11 +92,11 @@ def save_data(): # stp button is applyed the fucktion saves data to measurment f
             name = 'NoName'
         else:
             name = inputtxt.get()
-        data = pd.DataFrame({'1.0': t_val, 'pressure': pressure})
+        data = pd.DataFrame({'tine': t_val, 'pressure': pressure})
         data.to_excel(Path+'/' + name +'.xlsx')
-        Output.insert("1.0","Register measurements \n")
+        Output.insert("1.0","Register measurements to: \n"+Path+'/' + name +'.xlsx \n') # puts string a beginning of text field
     else:
-        Output.insert("1.0","Emty file \n")
+        Output.insert("1.0","Emty file \n") # puts string a beginning of text field
 
 # fuction that searches if and where a Arduino is connected
 def serialPortArduino(): # Searches if Arduino Uno is connecto or not
@@ -105,16 +108,16 @@ def serialPortArduino(): # Searches if Arduino Uno is connecto or not
     portVar = False
     portList = []
     if len(ports)==0:
-        Output.insert("1.0","No port is conected \n")
+        Output.insert("1.0","No port is conected \n") # puts string a beginning of text field
 
     for onePort in ports: # loops trough all connected ports and appends them to an array
         portName = str(onePort)
         portList.append(portName)
         if "Arduino Uno" in portName: # checks if one of the ports is an Arduino uno
             portVar = portName[0:5]
-            Output.insert("1.0","Arduino Uno conneced to Port: " + portVar + '\n')
+            Output.insert("1.0","Arduino Uno conneced to Port: " + portVar + '\n') # puts string a beginning of text field
         else:
-            Output.insert("1.0","No Arduino Uno conected \n")
+            Output.insert("1.0","No Arduino Uno conected \n") # puts string a beginning of text field
     return portVar # false or portname
 ########### Initialise the Serial port if Arduino is connected
 def comActive():
@@ -123,13 +126,13 @@ def comActive():
     if(portVar == False or comOpen==True):
         comOpen = False
         serialInst.close()
-        startSerial.config(image=off)
+        startSerial.config(image=off) # shows the off button
     else:
         serialInst.baudrate = 115200
         serialInst.port = portVar
         serialInst.open()
         serialInst.reset_input_buffer()
-        startSerial.config(image=on)
+        startSerial.config(image=on) # shows the ON button
         comOpen = True
 
 #------ Creat buttons of little user interface to control the plot starts etc
