040f1b1f62d8646c65779d4eba101f536b01e893
Simon <simon.knori@epfl.ch>
2022-10-10 14:53:29 -0700

    GUI works with baud rate setting but bug when wrog bud is set

--- Python_scripts/git_nid.py
+++ Python_scripts/git_nid.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import tkinter as tk #tkinter for GUI
 import pandas as pd # for writing into a .xml file
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,NavigationToolbar2Tk)
+import multiprocessing
 import time
 
 import numpy as np
@@ -34,12 +35,15 @@ def plotFunc(t,data):
     canvas.draw()
     # creating the Matplotlib toolbar
     root.update()
+
+
 # Fuction triggered by GUI
 def record_data(): # starts measurements when the start btton is applied
     t = 0
     global recordStop
     global t_val
     global pressure
+    global comOpen
     if(recordStop == True and comOpen != False):# Starts record when COM is open and recod is enabled by GUI
         recordStop =False
         plt.close()
@@ -59,8 +63,21 @@ def record_data(): # starts measurements when the start btton is applied
             break
         if serialInst.in_waiting: # waits until charcters recived from Arduino
             t = t + Ts
-            bites = serialInst.readline() # reads recived bytes
-            packet = str(bites.decode('utf')).replace('\r\n', '', 1) # decodes recived bytes and removes \n\r from the reading
+            ## Make function out of fhtis
+            try:
+                bites = serialInst.readline() # reads recived bytes
+            except:
+                recordStop=True
+                record.config(image=off)  # shows the ON button
+                Output.insert('1.0','Cant read line')
+                break
+            try:
+                packet = str(bites.decode('utf')).replace('\r\n', '',1)  # decodes recived bytes and removes \n\r from the reading
+            except:
+                record.config(image=off)  # shows the ON button
+                Output.insert('1.0', 'Cant decode line')
+                break
+            #########
             float_or_not = packet.replace('.', '', 1).isdigit() # checks if number is convertible to float
             if float_or_not == False: # if not float append 0 to data vector
                 pressure.append(0)
@@ -87,10 +104,10 @@ def OnOff_plot():
 
 def save_data(): # stp button is applyed the fucktion saves data to measurment file
     if (len(t_val)>0):
-        if(inputtxt.get()==''):
+        if(fileName.get()== ''):
             name = 'NoName'
         else:
-            name = inputtxt.get()
+            name = fileName.get()
         data = pd.DataFrame({'tine': t_val, 'pressure': pressure})
         data.to_excel(Path+'/' + name +'.xlsx')
         Output.insert("1.0","Register measurements to: \n"+Path+'/' + name +'.xlsx \n') # puts string a beginning of text field
@@ -126,78 +143,95 @@ def comActive():
         comOpen = False
         serialInst.close()
         startSerial.config(image=off) # shows the off button
+        Output.insert('1.0','Serial port is closed or inexisten \n')
     else:
-        serialInst.baudrate = 115200
-        serialInst.port = portVar
-        serialInst.open()
-        serialInst.reset_input_buffer()
-        startSerial.config(image=on) # shows the ON button
-        comOpen = True
+        try:
+            serialInst.baudrate = int(baudText.get())
+            serialInst.port = portVar
+            serialInst.timeout = 1
+            serialInst.open()
+            serialInst.reset_input_buffer()
+            startSerial.config(image=on) # shows the ON button
+            Output.insert("1.0", "Baudrate is set to:" + baudText.get() + "\n")
+            comOpen = True
+        except:
+            Output.insert("1.0", "Baudrate is not correct" + baudText.get() + "\n")  # puts string a beginning of text field("")
 
-#------ Creat buttons of little user interface to control the plot starts etc
-root = tk.Tk()
-root.title('Real time plot')
-root.config(background= 'light blue')
-root.geometry('1000x600') #set the window size
 
-#define fgures
-on = tk.PhotoImage(file=Path + "/Python_scripts/on.png")
-off = tk.PhotoImage(file=Path + "/Python_scripts/off.png")
+if __name__ == '__main__':
+    #------ Creat buttons of little user interface to control the plot starts etc
+    root = tk.Tk()
+    root.title('Real time plot')
+    root.config(background= 'light blue')
+    root.geometry('1000x600') #set the window size
 
-# Register data button
-serialLabel = tk.Label(root, text="Serial On/Off", font= ('calbiri', 15))
-serialLabel.place(x = 10, y = 20)
-root.update()
-startSerial = tk.Button(root, image=off, font= ('calbiri',12), command= lambda: comActive()) #text= "Serial On/Off",
-startSerial.place(x= serialLabel.winfo_x()+serialLabel.winfo_width()+20, y=10)
+    #define fgures
+    on = tk.PhotoImage(file=Path + "/Python_scripts/on.png")
+    off = tk.PhotoImage(file=Path + "/Python_scripts/off.png")
 
-# Start Button
-root.update()
-recordLabel = tk.Label(root, text="Record On/Off", font= ('calbiri', 15))
-recordLabel.place(x = 10, y = serialLabel.winfo_y()+50)
-root.update()
-record = tk.Button(root, image=off,  command= lambda: record_data())
-record.place(x= recordLabel.winfo_x()+recordLabel.winfo_width()+20, y=recordLabel.winfo_y()-8)
+    # Serial comunication start button
+    serialLabel = tk.Label(root, text="Serial On/Off", font= ('calbiri', 15))
+    serialLabel.place(x = 10, y = 20)
+    root.update()
+    startSerial = tk.Button(root, image=off, font= ('calbiri',12), command= lambda: comActive()) #text= "Serial On/Off",
+    startSerial.place(x= serialLabel.winfo_x()+serialLabel.winfo_width()+20, y=10)
 
-# Plot on off
-plotLabel = tk.Label(root, text="Plot On/Off", font= ('calbiri', 15))
-plotLabel.place(x = 10, y = recordLabel.winfo_y()+50)
-root.update()
-plotOnOff = tk.Button(root, image=off, font= ('calbiri',15), command= lambda: OnOff_plot())
-plotOnOff.place(x= plotLabel.winfo_x()+plotLabel.winfo_width()+20, y= plotLabel.winfo_y()-8)
+    # tell GUI the baude rate
+    root.update()
+    baudLabel = tk.Label(root, text="Set Baudrate",font=("Helvetica", 15))
+    baudLabel.place(x = 10, y = serialLabel.winfo_y()+50)
+    root.update()
+    baudText = tk.Entry(root, width=20, borderwidth=2,font=("Helvetica", 15)) #bg="black", fg="white",
+    baudText.place(x=baudLabel.winfo_x() + baudLabel.winfo_width() + 20, y=baudLabel.winfo_y())
+    root.update()
 
-# save data button
-root.update()
-save = tk.Button(root, text= "Save", font= ('calbiri',12), command= lambda: save_data())
-save.place(x=10, y=plotLabel.winfo_y() + 40)
+    # record data start button
+    root.update()
+    recordLabel = tk.Label(root, text="Record On/Off", font= ('calbiri', 15))
+    recordLabel.place(x = 10, y = baudLabel.winfo_y()+50)
+    root.update()
+    record = tk.Button(root, image=off,  command= lambda: record_data())
+    record.place(x= recordLabel.winfo_x()+recordLabel.winfo_width()+20, y=recordLabel.winfo_y()-8)
 
-# tell ui the name of the saved file
-root.update()
-inputtxt = tk.Entry(root,width=20, bg="black", fg="white", borderwidth=5)
-inputtxt.place(x=save.winfo_x()+save.winfo_width()+20,y=save.winfo_y()+2)
-root.update()
+    # Plot on off
+    plotLabel = tk.Label(root, text="Plot On/Off", font= ('calbiri', 15))
+    plotLabel.place(x = 10, y = recordLabel.winfo_y()+50)
+    root.update()
+    plotOnOff = tk.Button(root, image=off, font= ('calbiri',15), command= lambda: OnOff_plot())
+    plotOnOff.place(x= plotLabel.winfo_x()+plotLabel.winfo_width()+20, y= plotLabel.winfo_y()-8)
 
-#------ puttig the text log field ----
-Output = tk.Text(root, height=20,width=30,bg="light cyan")
-Output.place(x=10, y=inputtxt.winfo_y()+inputtxt.winfo_height() + 10 )
-root.update()
+    # save data button
+    root.update()
+    save = tk.Button(root, text= "Save", font= ('calbiri',15), command= lambda: save_data())
+    save.place(x=10, y=plotLabel.winfo_y() + 50)
 
-#------The figure on the user interface window------#
-fig = plt.figure()
-plot1 = fig.add_subplot(111)
-plot1.set_title('Serial Data')
-plot1.set_xlabel('t')
-plot1.set_ylabel('data')
-plot1.grid()
-# creating the Tkinter canvas
-# containing the Matplotlib figure
-canvas = FigureCanvasTkAgg(fig,master=root)
-canvas.get_tk_widget().place(x=record.winfo_x()+record.winfo_width()+20 , y=startSerial.winfo_y()) # placing the canvas on the Tkinter window
-canvas.draw()
-toolbar = NavigationToolbar2Tk(canvas,root)
-toolbar.update()
-root.update()
+    # tell ui the name of the saved file
+    root.update()
+    fileName = tk.Entry(root, width=20, borderwidth=2, font=("Helvetica", 15))
+    fileName.place(x=save.winfo_x() + save.winfo_width() + 20, y=save.winfo_y()+5 )
+    root.update()
 
-root.mainloop()
+    #------ puttig the text log field ----
+    Output = tk.Text(root, height=20,width=35,bg="light cyan",font=("Helvetica", 15))
+    Output.place(x=10, y=fileName.winfo_y() + fileName.winfo_height() + 10)
+    root.update()
+
+    #------The figure on the user interface window------#
+    fig = plt.figure()
+    plot1 = fig.add_subplot(111)
+    plot1.set_title('Serial Data')
+    plot1.set_xlabel('t')
+    plot1.set_ylabel('data')
+    plot1.grid()
+    # creating the Tkinter canvas
+    # containing the Matplotlib figure
+    canvas = FigureCanvasTkAgg(fig,master=root)
+    canvas.get_tk_widget().place(x=baudText.winfo_x()+baudText.winfo_width()+20 , y=startSerial.winfo_y()) # placing the canvas on the Tkinter window
+    canvas.draw()
+    toolbar = NavigationToolbar2Tk(canvas,root)
+    toolbar.update()
+    root.update()
+
+    root.mainloop()
 
 
