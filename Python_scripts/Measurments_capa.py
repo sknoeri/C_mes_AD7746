1f6ecc2e71b2cf0468c6b24fd69c58b6502bbfa0
Simon <simon.knori@epfl.ch>
2022-11-08 08:58:14 -0800

    make plot of the measurments with python

--- Python_scripts/Measurments_capa.py
+++ Python_scripts/Measurments_capa.py
@@ -1,61 +1,10 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import serial.tools.list_ports
-import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-# initalize the serial port
-ports = serial.tools.list_ports.comports()
-serialInst = serial.Serial()
-portList = []
-portVar = False
-pressure = []
-time = []
-t = 0
-cond = True
-plt.ion()
-alpfhabet = ['a','b','c','d','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','+','!','*','?','/','$']
-
-def plot():
-    plt.title('Serial value from Arduino')
-    plt.grid(True)
-    plt.ylabel('Values')
-    plt.xlabel('time')
-    plt.plot(time,pressure)
-    plt.show()
-    plt.pause(0.01)
-
-if len(ports)==0:
-    print("No port is conected")
-for onePort in ports:
-    portName = str(onePort)
-    portList.append(portName)
-    print(portName)
-    if "Arduino Uno" in portName:
-        portVar = portName[0:5]
-        print("Arduino Uno conneced to Port: " + portVar)
-    else:
-        print("No Arduino Uno conected")
-    if portVar:
-        serialInst.baudrate = 115200
-        serialInst.port = portVar
-        serialInst.open()
-        serialInst.reset_input_buffer()
-        while True:
-            if serialInst.in_waiting:
-                t = t+1
-                bites = serialInst.readline()
-                packet = str(bites.decode('utf')).replace('\r\n','',1)
-                float_or_not = packet.replace('.','', 1).isdigit()
-                if float_or_not == False:
-                    pressure.append(0)
-                    time.append(t)
-                    print(packet)
-                else:
-                    pressure.append(float(packet))
-                    time.append(t)
-                plot()
-
-
+path = '/Users\simon\Documents\Simels_daten\Epfl\sem_13_2022_Master_theis_USA\Master_thesis\Capacitance_measuring\C_mes_AD7746'
+meas=pd.read_excel(open(path+'/2mmcpa_Oil348DI174_droplets_FF2.xlsx', 'rb'))
+time = meas['time'].to_numpy()
+capa = meas['pressure'].to_numpy()
+plt.plot(time,capa)
+plt.show()
