62b68c60e4011764c077e5efc46dbcaf989f4d3b
Simon <simon.knori@epfl.ch>
2023-01-19 18:13:09 -0800

    analyzed measurments results

--- Python_scripts/Measurments_CCs-LIMMPET.py
+++ Python_scripts/Measurments_CCs-LIMMPET.py
@@ -3,21 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 path = '/Users\simon\Documents\Simels_daten\Epfl\sem_13_2022_Master_theis_USA\Master_thesis\Capacitance_measuring\Measurments_excel\Measures_CCS-LIMMPET'
-meas=pd.read_excel(open(path+'/CCs_LIMMPET1um_try_1_3_1.5baddrplets.xlsx', 'rb'))
+meas=pd.read_excel(open(path+'/CCs_LIMMPET_CapaSerie_randomSpeed2_1um.xlsx', 'rb'))
 timeA = meas['timeA'].to_numpy()
-timeB = meas['timeB'].to_numpy()
-timeV = meas['timeV'].to_numpy()
+# timeB = meas['timeB'].to_numpy()
+# timeV = meas['timeV'].to_numpy()
 capaA = meas['A chanel'].to_numpy()
-capaB = meas['B channel'].to_numpy()
-Volts = meas['Volts'].to_numpy()
+# capaB = meas['B channel'].to_numpy()
+# Volts = meas['Volts'].to_numpy()
 
 
 ## making the plots of the measurements
 plt.figure()
 
 plt.plot(timeA, capaA, 'b', label='Sense A C[fF]')
-plt.plot(timeB, capaB, 'r', label='Sense B C[fF]')
-plt.plot(timeV, Volts, 'g', label='Volts [mV]')
+# plt.plot(timeB, capaB+400, 'r', label='Sense B C[fF]')
+# plt.plot(timeV, Volts, 'g', label='Volts [mV]')
 plt.ylabel('C [fF]')
 plt.xlabel('t [s]')
 plt.legend()
